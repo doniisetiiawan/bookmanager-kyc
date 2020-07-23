c1414cb3bb8782e7d47d23da4991120e06022217
Doni Setiawan <donisetiawan.050@gmail.com>
2020-07-23 14:28:16 +0700

    The App component and state

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,26 +1,51 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import {
+  BrowserRouter as Router,
+  NavLink,
+  Route,
+  Switch,
+} from 'react-router-dom';
 import logo from '../logo.svg';
 import './App.css';
+import Home from './home';
 
-function App() {
+function App(props) {
+  const { title } = props;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />{' '}
+          <h1 className="App-title">{title}</h1>
+        </header>
+        <section className="Layout">
+          <nav>
+            <NavLink
+              exact
+              to="/"
+              activeStyle={{ fontWeight: 'bold' }}
+            >
+              Home
+            </NavLink>
+            <NavLink
+              to="/new"
+              activeStyle={{ fontWeight: 'bold' }}
+            >
+              New Book
+            </NavLink>
+          </nav>
+          <section>
+            <Switch>
+              <Route exact path="/" component={Home} />
+            </Switch>
+          </section>
+        </section>
+      </div>
+    </Router>
   );
 }
 
-export default App;
+const mapState = (state) => state.app;
+const mapDispatch = () => ({});
+export default connect(mapState, mapDispatch)(App);
